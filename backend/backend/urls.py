557d72e257fbfac099d1926b912465a2f250eec3
Aleksandra Nikolov <nikolov.aleksandra@hotmail.com>
2020-10-16 11:52:57 +0200

    Commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,14 +22,14 @@ from rest_framework_simplejwt import views as jwt_views
 
 
 authurls = [
-    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_refresh'),
+    path('backend/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('backend/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('backend/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_refresh'),
 ]
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('backend/admin/', admin.site.urls),
     path('backend/api/social/posts/', include('posts.urls')),
     path('backend/api/users/', include('users.urls')),
     path('backend/api/social/followers/', include('users.urls')),
